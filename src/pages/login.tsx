b410bc6b3fa5ddaf4091fe3e05c145fdb36bcd90
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-02-01 18:06:25 +0530

    intial setup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useLogin } from "@refinedev/core";
 import { useEffect, useRef } from "react";
 
@@ -7,10 +8,9 @@ import Typography from "@mui/material/Typography";
 import { ThemedTitleV2 } from "@refinedev/mui";
 
 import { CredentialResponse } from "../interfaces/google";
+import { yariga } from '../assets';
 
 // Todo: Update your Google Client ID here
-const GOOGLE_CLIENT_ID =
-  "1041339102270-e1fpe2b6v6u1didfndh7jkjmpcashs4f.apps.googleusercontent.com";
 
 export const Login: React.FC = () => {
   const { mutate: login } = useLogin<CredentialResponse>();
@@ -26,7 +26,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
+          client_id: import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -47,40 +47,34 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <Container
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <Box
+        sx={{
+            backgroundColor: '#fcfcfc'
+        }}
     >
-      <Box
-        display="flex"
-        gap="36px"
-        justifyContent="center"
-        flexDirection="column"
-      >
-        <ThemedTitleV2
-          collapsed={false}
-          wrapperStyles={{
-            fontSize: "22px",
+        <Container
+        style={{
+            height: "100vh",
+            display: "flex",
             justifyContent: "center",
-          }}
-        />
+            alignItems: "center",
+            }}
+            >
 
-        <GoogleButton />
-
-        <Typography align="center" color={"text.secondary"} fontSize="12px">
-          Powered by
-          <img
-            style={{ padding: "0 5px" }}
-            alt="Google"
-            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg"
-          />
-          Google
-        </Typography>
-      </Box>
-    </Container>
+            <Box
+                display="flex"
+                gap="36px"
+                justifyContent="center"
+                flexDirection="column"
+                >
+                <div>
+                    <img src={yariga} alt="Yariga Logo" />
+                </div>
+                <Box>
+                    <GoogleButton />
+                </Box>
+            </Box>
+        </Container>
+    </Box>
   );
 };
